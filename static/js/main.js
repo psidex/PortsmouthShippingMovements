9e903bd8634d97c962869207492d16bfb32601fe
thatguywiththatname <simjenner3@gmail.com>
2020-09-11 20:36:31 +0100

    Update frontend hmtl js & css

--- static/js/main.js
+++ static/js/main.js
@@ -1,75 +1,64 @@
-// TODO: Make HMS ships stand out more, make other vessels take up less screen space.
-
 const apiRoute = '/api/movements';
 
-function fromToSentence(day) {
-    if (day.from.name !== '' && day.to.name !== '') {
-        return `${day.from.name} to ${day.to.name}`;
+function fromToSentence(movement) {
+    if (movement.type === 0) {
+        return `Moving from ${movement.from.name} to ${movement.to.name}`;
     }
     return '';
 }
 
-function createCard(day) {
-    const card = document.createElement('div');
-    card.setAttribute('class', 'card horizontal');
-
-    const cardImage = document.createElement('div');
-    const cardImageImg = document.createElement('img');
-    cardImage.setAttribute('class', 'card-image');
-    cardImageImg.setAttribute('src', day.imageUrl);
-    cardImage.appendChild(cardImageImg);
-
-    const cardStacked = document.createElement('div');
-    const cardContent = document.createElement('div');
-    cardStacked.setAttribute('class', 'card-stacked');
-    cardContent.setAttribute('class', 'card-content');
-
-    const titleH6 = document.createElement('h6');
-    const timeP = document.createElement('p');
-    const moveP = document.createElement('p');
-    titleH6.textContent = day.name;
-    timeP.textContent = day.time;
-    moveP.textContent = fromToSentence(day);
-
-    cardContent.appendChild(titleH6);
-    cardContent.appendChild(timeP);
-    cardContent.appendChild(moveP);
-    cardStacked.appendChild(cardContent);
-
-    card.appendChild(cardImage);
-    card.appendChild(cardStacked);
-    return card;
+function fromToAbbreviation(movement) {
+    if (movement.type === 0) {
+        return `${movement.from.abbreviation} to ${movement.to.abbreviation}`;
+    }
+    return '';
 }
 
-function addRow(today = undefined, tomorrow = undefined) {
-    const container = document.querySelector('.container');
-    const row = document.createElement('div');
-    row.setAttribute('class', 'row');
+function addLi(movement, collapsible) {
+    const movementLi = document.createElement('li');
 
-    if (today !== undefined) {
-        const colL = document.createElement('div');
-        colL.setAttribute('class', 'col s6');
-        const cardL = createCard(today);
-        colL.appendChild(cardL);
-        row.appendChild(colL);
+    const headerDiv = document.createElement('div');
+    const movementTitleP = document.createElement('p');
+    const fromToP = document.createElement('p');
+
+    headerDiv.setAttribute('class', 'collapsible-header');
+
+    movementTitleP.textContent = `${movement.time} - ${movement.name}`;
+    movementTitleP.setAttribute('class', 'movement-title');
+    if (!movement.name.startsWith('MV')) {
+        // All the ferries are "MV", anything not a ferry could be interesting.
+        movementTitleP.setAttribute('class', 'movement-title text-bold');
     }
 
-    if (tomorrow !== undefined) {
-        const colR = document.createElement('div');
-        if (today === undefined) {
-            colR.setAttribute('class', 'col s6 offset-s6');
-        } else {
-            colR.setAttribute('class', 'col s6');
-        }
-        const cardR = createCard(tomorrow);
-        colR.appendChild(cardR);
-        row.appendChild(colR);
-    }
+    fromToP.textContent = fromToAbbreviation(movement);
+    fromToP.setAttribute('class', 'right-margin');
 
-    container.appendChild(row);
+    headerDiv.appendChild(movementTitleP);
+    headerDiv.appendChild(fromToP);
+    movementLi.appendChild(headerDiv);
+
+    const bodyDiv = document.createElement('div');
+    const bodyImg = document.createElement('img');
+    const bodyP = document.createElement('p');
+
+    bodyDiv.setAttribute('class', 'collapsible-body');
+    bodyImg.setAttribute('src', movement.imageUrl);
+    bodyP.textContent = fromToSentence(movement);
+
+    bodyDiv.appendChild(bodyImg);
+    bodyDiv.appendChild(bodyP);
+    movementLi.appendChild(bodyDiv);
+
+    if (movement.name.startsWith('HMS')) {
+        movementLi.setAttribute('class', 'active');
+    }
+    collapsible.appendChild(movementLi);
 }
 
 document.addEventListener('DOMContentLoaded', async () => {
+    const todayCollapsible = document.querySelector('#todayCollapsible');
+    const tomorrowCollapsible = document.querySelector('#tomorrowCollapsible');
+
     const r = await fetch(apiRoute);
     const movements = await r.json();
 
@@ -77,13 +66,20 @@ document.addEventListener('DOMContentLoaded', async () => {
     for (; i < movements.today.length; i++) {
         const todayMovement = movements.today[i];
         const tomorrowMovement = movements.tomorrow[i];
-        addRow(todayMovement, tomorrowMovement);
+        addLi(todayMovement, todayCollapsible);
+        if (tomorrowMovement !== undefined) {
+            addLi(tomorrowMovement, tomorrowCollapsible);
+        }
     }
 
     if (movements.tomorrow.length > movements.today.length) {
         for (; i < movements.tomorrow.length; i++) {
             const tomorrowMovement = movements.tomorrow[i];
-            addRow(undefined, tomorrowMovement);
+            addLi(tomorrowMovement, tomorrowCollapsible);
         }
     }
+
+    // Init after all the HTML is setup.
+    const elems = document.querySelectorAll('.collapsible');
+    M.Collapsible.init(elems, {accordion: false});
 });
