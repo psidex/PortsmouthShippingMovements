2de56999f2f4676742baddec64d7cbc044a78c72
thatguywiththatname <simjenner3@gmail.com>
2020-09-14 14:55:56 +0100

    Actually loop updater function

--- internal/movements/udpater.go
+++ internal/movements/udpater.go
@@ -7,21 +7,23 @@ import (
 
 // UpdateMovementsPeriodically updates the 2 movement lists in the MovementStorage every sleepDuration.
 func UpdateMovementsPeriodically(movementStore *MovementStorage, scraper MovementScraper, sleepDuration time.Duration) {
-	log.Println("Updating movementStore todayMovements")
-	todayMovements, err := scraper.GetTodayMovements()
-	if err != nil {
-		log.Printf("Error when calling GetTodayMovements: %v", err)
-	} else {
-		movementStore.SetTodayMovements(todayMovements)
-	}
+	for {
+		log.Println("Updating movementStore todayMovements")
+		todayMovements, err := scraper.GetTodayMovements()
+		if err != nil {
+			log.Printf("Error when calling GetTodayMovements: %v", err)
+		} else {
+			movementStore.SetTodayMovements(todayMovements)
+		}
 
-	log.Println("Updating movementStore tomorrowMovements")
-	tomorrowMovements, err := scraper.GetTomorrowMovements()
-	if err != nil {
-		log.Printf("Error when calling GetTomorrowMovements: %v", err)
-	} else {
-		movementStore.SetTomorrowMovements(tomorrowMovements)
-	}
+		log.Println("Updating movementStore tomorrowMovements")
+		tomorrowMovements, err := scraper.GetTomorrowMovements()
+		if err != nil {
+			log.Printf("Error when calling GetTomorrowMovements: %v", err)
+		} else {
+			movementStore.SetTomorrowMovements(tomorrowMovements)
+		}
 
-	time.Sleep(sleepDuration)
+		time.Sleep(sleepDuration)
+	}
 }
