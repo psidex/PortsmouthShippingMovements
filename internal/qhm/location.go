0ead92c16b7ee3dce12c5b7a076a4135e1061214
thatguywiththatname <simjenner3@gmail.com>
2020-11-27 00:56:42 +0000

    Finish abbrv parsing

--- internal/qhm/location.go
+++ internal/qhm/location.go
@@ -8,64 +8,6 @@ import (
 	"unicode"
 )
 
-var (
-	nRegex = regexp.MustCompile(`\((| )N(| )\)`)
-	eRegex = regexp.MustCompile(`\((| )E(| )\)`)
-	sRegex = regexp.MustCompile(`\((| )S(| )\)`)
-	wRegex = regexp.MustCompile(`\((| )W(| )\)`)
-	cRegex = regexp.MustCompile(`\((| )C(| )\)`)
-)
-
-// Location holds the names for a single location, to be used in the Movement type.
-type Location struct {
-	Abbreviation string `json:"abbreviation"` // The abbreviation of the location.
-	Name         string `json:"name"`         // The full name of the location.
-}
-
-// newLocation creates a Location from an abbreviation.
-func newLocation(abbrv string) Location {
-	return Location{
-		Abbreviation: abbrv,
-		Name:         parseAbbreviation(abbrv),
-	}
-}
-
-func parseAbbreviation(abbrv string) string {
-	if len(abbrv) <= 0 {
-		return ""
-	}
-
-	parsed := ""
-
-	// Potentially a more efficient way of doing this, but benchmarking this I don't think it matters too much.
-	abbrv = nRegex.ReplaceAllString(abbrv, "(North)")
-	abbrv = eRegex.ReplaceAllString(abbrv, "(East)")
-	abbrv = sRegex.ReplaceAllString(abbrv, "(South)")
-	abbrv = wRegex.ReplaceAllString(abbrv, "(West)")
-	abbrv = cRegex.ReplaceAllString(abbrv, "(Centre)")
-
-	for _, w := range strings.Split(abbrv, " ") {
-		runed := []rune(w)
-		if first := runed[0]; unicode.IsNumber(first) {
-			berth, _ := strconv.Atoi(string(first))
-			parsed += fmt.Sprintf("Berth %d ", berth)
-			w = string(runed[1:])
-		}
-
-		// With the above berth number logic and also splitting on spaces, trim(w) might be "".
-		if strings.TrimSpace(w) != "" {
-			if full, ok := qhmAbbreviations[w]; ok {
-				parsed += full
-			} else {
-				parsed += w
-			}
-			parsed += " "
-		}
-	}
-
-	return strings.TrimSpace(parsed)
-}
-
 // qhmAbbreviations is a string:string map of QHM abbreviation to full text.
 var qhmAbbreviations = map[string]string{
 	"NAB":     "Nab Tower",
@@ -100,4 +42,59 @@ var qhmAbbreviations = map[string]string{
 	"TCL":     "Tank Cleaner",
 	"HORB":    "Hold Off Re-Berth",
 	"WIND":    "Wind Ship (Cold Move Using Tugs To Turn Ship And Re-Berth)",
+}
+
+// abbrvRx contains regex and replacement text for things in an abbreviation that need replacing this way.
+var abbrvRx = map[*regexp.Regexp]string{
+	regexp.MustCompile(`\((| )N(| )\)`): "(North)",
+	regexp.MustCompile(`\((| )E(| )\)`): "(East)",
+	regexp.MustCompile(`\((| )S(| )\)`): "(South)",
+	regexp.MustCompile(`\((| )W(| )\)`): "(West)",
+	regexp.MustCompile(`\((| )C(| )\)`): "(Centre)",
+}
+
+// Location holds the names for a single location, to be used in the Movement type.
+type Location struct {
+	Abbreviation string `json:"abbreviation"` // The abbreviation of the location.
+	Name         string `json:"name"`         // The full name of the location.
+}
+
+// newLocation creates a Location from an abbreviation.
+func newLocation(abbrv string) Location {
+	return Location{
+		Abbreviation: abbrv,
+		Name:         parseAbbreviation(abbrv),
+	}
+}
+
+func parseAbbreviation(abbrv string) string {
+	if len(abbrv) <= 0 {
+		return ""
+	}
+
+	// Potentially a more efficient way of doing this, but benchmarking this I don't think it matters too much.
+	for r, text := range abbrvRx {
+		abbrv = r.ReplaceAllString(abbrv, text)
+	}
+
+	parsed := ""
+	for _, w := range strings.Split(abbrv, " ") {
+		runed := []rune(w)
+		if first := runed[0]; unicode.IsNumber(first) {
+			berth, _ := strconv.Atoi(string(first))
+			parsed += fmt.Sprintf("Berth %d ", berth)
+			w = string(runed[1:])
+		}
+
+		// With the above berth number logic and also splitting on spaces, trim(w) might be "".
+		if strings.TrimSpace(w) != "" {
+			if full, ok := qhmAbbreviations[w]; ok {
+				parsed += full
+			} else {
+				parsed += w
+			}
+			parsed += " "
+		}
+	}
+	return strings.TrimSpace(parsed)
 }
